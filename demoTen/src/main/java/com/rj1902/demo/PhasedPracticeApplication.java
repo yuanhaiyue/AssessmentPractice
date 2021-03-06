cc6371770bc4962f73cce210141a3028bba67a1d
yuanhaiyue <1476017740@qq.com>
2021-03-06 14:02:14 +0800

    初始化

---
+++ demoTen/src/main/java/com/rj1902/demo/PhasedPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.rj1902.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhasedPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhasedPracticeApplication.class, args);
+    }
+
+}
