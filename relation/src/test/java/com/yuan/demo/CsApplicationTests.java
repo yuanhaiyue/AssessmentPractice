856d7c2b2eaaaaafaa8613e2a07b532f66fc4581
yuanhaiyue <1476017740@qq.com>
2021-03-15 14:16:44 +0800

    提交

---
+++ relation/src/test/java/com/yuan/demo/CsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuan.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
